b8430029f4a03b7b1e9fcdd4cb337fb98d387f9c
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2020-04-21 22:11:24 +0300

    Update result_mixed_graphics.py

--- result_mixed_graphics.py
+++ result_mixed_graphics.py
@@ -3,17 +3,17 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 import seaborn as sns
+
 
 if __name__ == "__main__":
 
-	n_clusters = 5
-
-	f, ax1 = plt.subplots(1,1)
+	(f, (ax1,ax2)) = plt.subplots(1, 2)
 
 	## FIRST PLOT ##
 
+	n_clusters = 5
+	
 	bwa_content1 = np.array([57.6, 64.8, 75.2, 79.1, 55.9])
 	sbg_content1 = np.array([58.3, 64.6, 75.3, 79.8, 56.0])
 	vg_content1 = np.array([58.1, 63.4, 75.7, 76.6, 54.4])
@@ -42,9 +42,9 @@ if __name__ == "__main__":
 	y2 = np.array([86.7] * 8)
 	y3 = np.array([100.0] * 8)
 	
-	ax1.fill_between(x, y0, y1, color='blue', alpha=0.3, label='First strain')
-	ax1.fill_between(x, y1, y2, color='green', alpha=0.3, label='Second strain')
-	ax1.fill_between(x, y2, y3, color='darkorange', alpha=0.3, label='Third strain')
+	ax1.fill_between(x, y0, y1, color='blue', alpha=0.3) # label='First strain'
+	ax1.fill_between(x, y1, y2, color='green', alpha=0.3) # , label='Second strain')
+	ax1.fill_between(x, y2, y3, color='darkorange', alpha=0.3) #, label='Third strain')
 
 	ax1.bar(index - bar_width, bwa_content1, bar_width, color='dodgerblue', edgecolor='black')
 	ax1.bar(index - bar_width, bwa_content2, bar_width, color='seagreen', bottom = bwa_content1, edgecolor='black')
@@ -59,9 +59,9 @@ if __name__ == "__main__":
 	ax1.bar(index + bar_width, vg_content3, bar_width, color='darkorange', bottom = vg_content1+vg_content2, edgecolor='black')
 
 	for i in range(1,6):
-		ax1.text(i-0.2, 2.0, 'BWA', color='white', ha='center', va='bottom', rotation=90)
-		ax1.text(i, 2.0, 'SBG', color='white', ha='center', va='bottom', rotation=90)
-		ax1.text(i+0.2, 2.0, 'VG', color='white', ha='center', va='bottom', rotation=90)
+		ax1.text(i-0.2, 1.0, 'BWA', color='white', fontweight='bold',ha='center', va='bottom', fontsize='x-small', rotation=90)
+		ax1.text(i, 9.0, 'SBG', color='white',fontweight='bold',ha='center', va='bottom', fontsize='x-small', rotation=90)
+		ax1.text(i+0.2, 16.0, 'VG', color='white',fontweight='bold',ha='center', va='bottom', fontsize='x-small', rotation=90)
 
 	ax1.legend(bbox_to_anchor=(1.05, 1.0), fontsize='small', loc='upper left')
 	ax1.title.set_text("Strain Content\n(Mixed Sample: 10:3:2)")
@@ -75,6 +75,45 @@ if __name__ == "__main__":
 	ax1.grid(which='minor', linestyle='--', linewidth='0.001', alpha=0.1, color='gray')
 	ax1.grid(True)
 
+	## SECOND PLOT ##
+	
+	sample = np.arange(0, 6)
+	
+	bwa_sa_before_est = np.array([0, 40, 66, 57, 31, 32])
+	bwa_sa_after_est = np.array([0, 94, 127, 121, 83, 80])
+	
+	sbg_sa_before_est = np.array([0, 26, 32, 32, 21, 31])
+	sbg_sa_after_est = np.array([0, 216, 284, 346, 384, 333])
+		
+	vg_sa_before_est = np.array([0, 227, 239, 234, 220, 221])
+	vg_sa_after_est = np.array([0, 280, 300, 297, 271, 266])
+
+	ax2.plot(sample, bwa_sa_after_est, color='navy', label='BWA + Estimation')
+	ax2.plot(sample, bwa_sa_before_est, color='navy', linestyle='--', label='BWA')
+	
+	ax2.plot(sample, sbg_sa_after_est, color='darkgreen', label='SBG + Estimation')
+	ax2.plot(sample, sbg_sa_before_est, color='darkgreen', linestyle='--', label='SBG')
+	
+	ax2.plot(sample, vg_sa_after_est, color='darkorange', label='VG + Estimation')
+	ax2.plot(sample, vg_sa_before_est, color='darkorange', linestyle='--', label='VG')
+	
+	ax2.title.set_text("S.agalactiae")
+	
+	ax2.set_xlabel('Experiments')
+	ax2.set_ylabel('Run time (sec)')
+	
+	ax2.set(xlim=(0, 5.1), ylim=(0, 500))
+	ax2.set_xticks(np.arange(1, 6))
+	ax2.set_yticks(np.arange(0, 500, 100))
+	ax2.set_xticklabels(['E1', 'E2', 'E3', 'E4', 'E5'])
+	ax2.minorticks_on()
+	
+	ax2.grid(which='major', linestyle='-', linewidth='0.05', alpha=0.3, color='black')
+	ax2.grid(which='minor', linestyle='--', linewidth='0.001', alpha=0.1, color='gray')
+	ax2.grid(True)
+
+	ax2.legend(loc='upper left', fontsize='x-small')
+	
 	plt.tight_layout()
 	plt.savefig('mixed_experiment.pdf', format='pdf')
 	# plt.show()
